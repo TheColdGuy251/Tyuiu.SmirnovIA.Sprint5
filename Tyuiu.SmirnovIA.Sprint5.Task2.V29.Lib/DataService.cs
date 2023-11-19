b3a90007238b676a453e868b578ab74b970cdc02
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-11-19 18:10:06 +0500

    Task2

---
+++ Tyuiu.SmirnovIA.Sprint5.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SmirnovIA.Sprint5.Task2.V29.Lib
+{
+    public class DataService : ISprint5Task2V29
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            if (fileExist)
+            {
+                File.Delete(path);
+            }
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            string str = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str += matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        str += matrix[i, j];
+                    }
+                }
+
+
+                if (i != rows - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+                str = "";
+                
+            }
+            return path;
+        }   
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint5.Task2.V29/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SmirnovIA.Sprint5.Task2.V29.Lib;
+namespace Tyuiu.SmirnovIA.Sprint5.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Смирнов И. А. | ИИПб-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Смирнов Илья Алексеевич | ИИПб-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[3, 3] {{ 9, 2, 5},
+                                         { 3, 2, 4},
+                                         { 2, 8, 8}};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}
