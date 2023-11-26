a83fa619743e083727f56ca89d74ba629ddbb8a2
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-11-26 16:50:41 +0500

    Task5

---
+++ Tyuiu.SmirnovIA.Sprint5.Task5.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SmirnovIA.Sprint5.Task5.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SmirnovIA.Sprint5.Task5.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58f8e816-8b0c-4af4-953c-914ce931211f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SmirnovIA.Sprint5.Task5.V10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.SmirnovIA.Sprint5.Task5.V10.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint5.Task5.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Смирнов И. А. | ИИПб-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Смирнов Илья Алексеевич | ИИПб-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine(@"* Дан файл С:\DataSprint5\InPutDataFileTask5V10.txt в котором есть        *");
+            Console.WriteLine("* набор значений. Найти сумму всех четных целых чисел в файле. Полученный *");
+            Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить до трёх *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V10.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма чётных целых чисел в файле = " + ds.LoadFromDataFile(path));
+            Console.ReadKey();
+        }
+    }
+}
